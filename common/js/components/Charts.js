b2ecca9b72fc9b2c88462555916bf12cbf8c7e81
André Santos <andrefs@andrefs.com>
2018-07-17 21:47:37 +0100

    minor

--- common/js/components/Charts.js
+++ common/js/components/Charts.js
@@ -73,44 +73,13 @@ class Charts extends Component {
       x: new Date(x.year+'-'+x.month), y: x.total
     }));
 
-    const data4 = {
-      datasets: [{
-        label: 'Games played',
-        data: stats.gamesByMonth.map(x => ({
-          x: new Date(x.year+'-'+x.month), y: x.total
-        })),
-        backgroundColor: '#4F81BD',
-        yAxisID: 'y-axis-0',
-        fill: false
-      }]
-    };
+    const values4 = stats.gamesByMonth.map(x => ({
+      x: new Date(x.year+'-'+x.month), y: x.total
+    }));
 
-    const options4 = {
-      scales: {
-        yAxes: [{
-          position: 'left',
-          'id': 'y-axis-0',
-          ticks: {
-            beginAtZero:true
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Games played'
-          }
-        }],
-        xAxes: [{
-          offset: true,
-          type: 'time',
-          time: {
-            unit: 'month'
-          },
-          ticks: {
-            source: 'data'
-          }
-        }]
-      }
-    };
 
+console.log('XXXXXXXXXXXXXXX values1', values1);
+console.log('XXXXXXXXXXXXXXX values3', values3);
 
     const labels = [];
     const values = [];
@@ -149,12 +118,12 @@ class Charts extends Component {
             ylabel="Registered players"
             title="Registered players over time"
           />
-          <Col lg="6">
-              <h4 className="text-center">Games played over time</h4>
-              <div className="chart registeredPlayers-time">
-                  <Bar data={data4} options={options4} />
-              </div>
-          </Col>
+          <BarChart
+            values={values4}
+            chartClass="registeredPlayers-time"
+            ylabel="Games played"
+            title="Games played over time"
+          />
           <DoughnutChart
             values={values}
             labels={labels}
