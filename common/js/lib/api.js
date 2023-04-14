52b380d0d854d5faed609aa14c32ae382362a7eb
André Santos <andrefs@andrefs.com>
2023-04-14 23:35:38 +0100

    wip

--- common/js/lib/api.js
+++ common/js/lib/api.js
@@ -1,30 +1,27 @@
-import isomorphicFetch from 'isomorphic-fetch';
+import isomorphicFetch from "isomorphic-fetch";
 
-//const apiUrl = 'http://localhost:15111';
-const apiUrl = 'https://stars-api.andrefs.com';
+const apiUrl = "http://localhost:15111";
+//const apiUrl = 'https://stars-api.andrefs.com';
 //const userId = '5af61777396666c67abc4707'; // user1
-const userId = '5afc549d2904cc717e1090a4'; // user2
-
+//const userId = "5afc549d2904cc717e1090a4"; // user2
 
 // Overrides the fetch() method to add the base API url to the front.
 export const fetch = (url, params, ...rest) => {
-  return isomorphicFetch( apiUrl + url, {mode: 'cors', ...params}, ...rest);
+  return isomorphicFetch(apiUrl + url, { mode: "cors", ...params }, ...rest);
 };
 
 export const getStats = (...params) => {
-  const url = '/stats?token=ichbintherealOG';
+  const url = "/stats?token=ichbintherealOG";
   return fetch(url, ...params);
 };
-
 
 export const getUsers = (...params) => {
-  const url = '/users?token=ichbintherealOG';
+  const url = "/users?token=ichbintherealOG";
   return fetch(url, ...params);
 };
-
 
 export const getOrderedPairs = (...params) => {
-  const url = '/pairs?limit=100&orderedOnly=true&includeItems=true&token=ichbintherealOG';
+  const url =
+    "/pairs?limit=100&orderedOnly=true&includeItems=true&token=ichbintherealOG";
   return fetch(url, ...params);
 };
-
